88dfbce42c86b08ba12e02317dfe2b13318ec4a6
DarshitAnadakat <110161814+DarshitAnadakat@users.noreply.github.com>
2024-08-09 23:11:54 +0530

    Update Gross_Salary.java

--- Gross_Salary.java
+++ Gross_Salary.java
@@ -6,34 +6,35 @@ public class DarshitSalary {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter your salary: ");
-        double salary = scanner.nextDouble();
+        double darshitsalary = scanner.nextDouble();
 
-        if (salary < 0) {
+        
+        if (darshitsalary < 0) {
             System.out.println("Invalid input. Salary cannot be negative.");
         } else {
             double da = 0, hra = 0, ta = 0, others = 0, pf = 0;
 
-            if (salary < 25000) {
-                da = salary * 0.05;
-                hra = salary * 0.05;
-                ta = salary * 0.05;
-                others = salary * 0.05;
+            if (darshitsalary < 25000) {
+                da = darshitsalary * 0.05;
+                hra = darshitsalary * 0.05;
+                ta = darshitsalary * 0.05;
+                others = darshitsalary * 0.05;
                 pf = 1200;
-            } else if (salary < 50000) {
-                da = salary * 0.15;
-                hra = salary * 0.10;
-                ta = salary * 0.05;
-                others = salary * 0.03;
+            } else if (darshitsalary < 50000) {
+                da = darshitsalary * 0.15;
+                hra = darshitsalary * 0.10;
+                ta = darshitsalary * 0.05;
+                others = darshitsalary * 0.03;
                 pf = 1500;
             } else {
-                da = salary * 0.25;
-                hra = salary * 0.15;
-                ta = salary * 0.07;
-                others = salary * 0.05;
+                da = darshitsalary * 0.25;
+                hra = darshitsalary * 0.15;
+                ta = darshitsalary * 0.07;
+                others = darshitsalary * 0.05;
                 pf = 2000;
             }
 
-            double grossSalary = salary - (da + hra + ta + others + pf);
+            double grossSalary = darshitsalary - (da + hra + ta + others + pf);
 
             System.out.println("Gross Salary after deductions: " + grossSalary);
         }
